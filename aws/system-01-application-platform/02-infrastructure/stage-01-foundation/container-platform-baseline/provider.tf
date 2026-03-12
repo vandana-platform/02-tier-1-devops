a11135b916a1ea8563a3de3514d79c2277a4809b
Vandana T <vandana.platformops@gmail.com>
2026-03-12 11:33:07 -0500

    feat:Rename the folder to specific s3

--- aws/system-01-application-platform/02-infrastructure/stage-01-foundation/container-platform-baseline/provider.tf
+++
@@ -1,7 +0,0 @@
-# provider.tf
-# Configures the AWS provider used by Terraform.
-# This defines which AWS region Terraform will deploy resources into.
-
-provider "aws" {
-  region = var.aws_region
-}
--- aws/system-01-application-platform/02-infrastructure/stage-01-foundation/container-platform-baseline/variables.tf
+++
@@ -1,8 +0,0 @@
-# variables.tf
-# Defines input variables used by the container platform baseline infrastructure
-
-variable "aws_region" {
-  description = "AWS region where resources will be deployed"
-  type        = string
-  default     = "us-east-1"
-}
